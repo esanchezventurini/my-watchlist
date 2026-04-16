768ae2740ac8a640af8ebf044547f75c7ecc1ad5
esanchezventurini <ezeq.sv2000@gmail.com>
2026-04-16 00:16:24 -0300

    Add dockerfile and docker compose

--- my_watchlist_api/app/core/config.py
+++ my_watchlist_api/app/core/config.py
@@ -3,13 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "My Watchlist API"
-    database_url: str = "sqlite:///./my_watchlist.db"
+    database_url: str
 
-    jwt_secret_key: str = "testsecretkey"
-    jwt_algorithm: str = "HS256"
-    jwt_access_token_expire_minutes: int = 30
+    jwt_secret_key: str
+    jwt_algorithm: str
+    jwt_access_token_expire_minutes: int
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 
 settings = Settings()
